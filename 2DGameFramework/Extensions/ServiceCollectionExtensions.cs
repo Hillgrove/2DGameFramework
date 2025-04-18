c0e2d9482d4972ff844794244e09202f15bbf032
Jesper Højlund <github@hillgrove.dk>
2025-04-18 22:59:59 +0200

    Add DI support and refactor ServiceCollectionExtensions

--- 2DGameFramework/Extensions/ServiceCollectionExtensions.cs
+++ 2DGameFramework/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,38 @@
-﻿namespace _2DGameFramework.Extensions
+﻿using _2DGameFramework.Combat;
+using _2DGameFramework.Core;
+using _2DGameFramework.Logging;
+using _2DGameFramework.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace _2DGameFramework.Extensions
 {
-    internal class ServiceCollectionExtensions
+    /// <summary>
+    /// Provides an extension method to register all 2DGameFramework services.
+    /// </summary>
+    public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds all necessary framework services to the service collection.
+        /// </summary>
+        /// <param name="services">The service collection to which services will be added.</param>
+        /// <returns>The updated service collection.</returns>
+        public static IServiceCollection Add2DGameFrameWork(this IServiceCollection services)
+        {
+            // Logging
+            services.AddSingleton<ILogger>(new GameLoggerAdapter(GameLogger.Trace));
+
+            // Core services
+            services.AddSingleton<IDamageCalculator, DamageCalculator>();
+            services.AddTransient<IInventory, InventoryService>();
+
+            // Factories
+            // TODO: Fix
+            //services.AddSingleton<ICreatureFactory, CreatureFactory>();
+
+            // World
+            services.AddSingleton<World>();
+
+            return services;
+        }
     }
 }
