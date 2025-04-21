6303f85b3636a462165d053aa37d007bc89c4152
Jesper Højlund <github@hillgrove.dk>
2025-04-21 17:20:26 +0200

    Add decorator pattern for damage sources and weapons

---
+++ 2DGameFramework/Domain/Items/Decorators/DamageSourceDecorator.cs
@@ -0,0 +1,23 @@
+﻿using _2DGameFramework.Core;
+using _2DGameFramework.Interfaces;
+
+namespace _2DGameFramework.Domain.Items.Decorators
+{
+    /// <summary>
+    /// Base decorator for any IDamageSource (weapons, traps, etc.).
+    /// </summary>
+    public abstract class DamageSourceDecorator : IDamageSource, IItem
+    {
+        protected readonly IDamageSource _inner;
+        
+        public DamageType DamageType => _inner.DamageType;
+        public virtual int BaseDamage => _inner.BaseDamage;
+        public string Name => ((IItem)_inner).Name;
+        public string Description => ((IItem)_inner).Description;
+
+        protected DamageSourceDecorator(IDamageSource inner)
+        {
+            _inner = inner;
+        }
+    }
+}
---
+++ 2DGameFramework/Domain/Items/Decorators/WeaponDecorator.cs
@@ -0,0 +1,22 @@
+﻿using _2DGameFramework.Core;
+using _2DGameFramework.Interfaces;
+
+namespace _2DGameFramework.Domain.Items.Decorators
+{
+    public abstract class WeaponDecorator : DamageSourceDecorator, IWeapon
+    {
+        protected readonly IWeapon _innerWeapon;
+
+        public int Range => _innerWeapon.Range;
+
+        public WeaponType WeaponType => _innerWeapon.WeaponType;
+
+
+        public WeaponDecorator(IWeapon inner) : base(inner)
+        {
+            _innerWeapon = inner;
+        }
+
+
+    }
+}
